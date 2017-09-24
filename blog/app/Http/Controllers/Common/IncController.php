1cd4ac51026b4a219b3ca0370f4722c2c6637bb7
leon <616649679@qq.com>
2017-09-24 22:22:41 +0800

    后台架构

---
+++ blog/app/Http/Controllers/Common/IncController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Common;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IncController extends Controller
+{
+    public $adminAllPower = [];             //存储所有URL
+    public function navList()
+    {
+        return $this->adminAllPower = array(
+                '商品管理'=>array(
+                    '商品分类' => array(
+                        'url' => '1.php',
+                        'show' => '1',
+                    ),
+                    '修改商品' => array(
+                        'url' => '2.php',
+                        'show' => '0',
+                    ),
+                    '商品详情' => array(
+                        'url' => '3.php',
+                        'show' => '1',
+                    ),
+                ),
+
+
+                '订单管理'=>array( 
+                    '订单列表' => array(
+                        'url' => '4.php',
+                        'show'=> '1'
+                    ),
+                    '生成订单' => array(
+                        'url' => '5.php',
+                        'show'=> '1'
+                    )
+                ),
+
+
+            '管理员管理' => array(
+                '权限管理' => array(
+                    'url' => 'admin/Administrator/power',
+                    'show' => '1',
+                ),
+                '修改权限' => array(
+                    'url' => 'admin/Administrator/editPower',
+                    'show' => '0'
+                    ),
+                ),
+
+
+        );
+    }
+}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,16 +11,46 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/12', function () {
-    echo 'hello';
-});
+Route::get('/', function() {
+		return view('welcome');
+	}
+);
+//Home
 
-//===========================================
+
+
+
+
+//===============================================
 //Admin
-//get
-Route::get('/admin/login', 'Admin\IndexController@login');
-Route::get('/admin/', 'Admin\IndexController@index');
+
+
+/***********后台登录模块***********/
+//加载登录页面
+Route::get('/admin/login', 'Admin\LoginController@login');
+//登录处理
+Route::post('/admin/doLogin', 'Admin\LoginController@doLogin');
+
+/***********后台主页模块***********/
+//加载后台主页
+Route::get('/admin/', 'Admin\IndexController@index')->middleware('adminSession');
+//头部
+Route::get('/admin/Index/head', 'Admin\IndexController@head')->middleware('adminSession');
+//侧面菜单
+Route::get('/admin/Index/nav', 'Admin\IndexController@nav')->middleware('adminSession');
+//底部
+Route::get('/admin/Index/footer', 'Admin\IndexController@footer')->middleware('adminSession');
+//注销
+Route::get('/admin/logout', 'Api\Admin\LoginApi@logout')->middleware('adminSession');
+
+
+/***********管理员管理模块***********/
+//加载管理权限页面
+Route::get('/admin/Administrator/power', 'Admin\AdministratorController@power');
+//修改权限页面
+Route::get('/admin/Administrator/editPower', 'Admin\AdministratorController@editPower')->middleware('adminPower');
+//执行修改权限
+Route::post('/admin/Administrator/updatePower', 'Api\Admin\AdministratorApi@updatePower');
+
+
 
